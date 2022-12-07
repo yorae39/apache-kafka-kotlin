59377af15ee2a436115cde8115bcce990a74be92
lpa0700 <luiz.aureliano@zoop.com.br>
2022-12-07 01:18:11 -0300

    str-producer started

---
+++ str-producer/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "str-producer"
---
+++ str-producer/src/main/kotlin/com/example/strproducer/StrProducerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.strproducer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StrProducerApplication
+
+fun main(args: Array<String>) {
+    runApplication<StrProducerApplication>(*args)
+}
